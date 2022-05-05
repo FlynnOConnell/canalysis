b58e6c0267b9d0aa38b1ac0882d8edb4142a1883
Flynn P O'Connell <oconnell@binghamton.edu>
2022-05-05 01:58:00 -0400

    reqs

--- CalciumAnalysis/main.py
+++ CalciumAnalysis/main.py
@@ -7,8 +7,9 @@ Module: Main code execution.
         Note: Neural network requires separate main.py in neuralnetwork subpackage.
 """
 import logging
+import yaml
 
-from data.calciumdata import CalciumData
+from data.calcium_data import CalciumData
 
 from data.data_utils.file_handler import FileHandler
 from data.taste_data import TasteData
@@ -16,23 +17,29 @@ from data.taste_data import TasteData
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 logger = logging.getLogger(__name__)
 
+with open('defaults.yml', 'r') as c:
+    config = yaml.safe_load(c)
+
+datadir = config['DIRS']
 
 
 # %% Some examples on usage of different modules.
 
 def initialize_data():
     """Set directory where data is stored."""
-    
+
     datadir = '/Users/flynnoconnell/Documents/Work/Data'
     animal = 'PGT13'
     date = '121021'
     data = CalciumData(animal, date, datadir)
     return data
 
-def sparese_event_data(data):
+
+def sparse_event_data(ev_data):
     """Get specific subset of data based on particular events"""
-    taste_data = TasteData(data.tracedata, data.timestamps, data.color_dict)
+    taste_data = TasteData(ev_data.tracedata, ev_data.timestamps, ev_data.color_dict)
     return taste_data
+
 
 datadir = '/Users/flynnoconnell/Documents/Work/Data'
 animal = 'PGT13'
@@ -42,4 +49,4 @@ handler.eventname = 'gpio'
 handler.tree()
 
 if __name__ == "__main__":
-   data = initialize_data()
+    data = initialize_data()
