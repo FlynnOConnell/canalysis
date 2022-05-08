d83eaa716066b2920889b6841bd29ff6232aa5ca
Flynn P O'Connell <oconnell@binghamton.edu>
2022-05-08 14:28:02 -0400

    parse tracedata to sig and time

--- CalciumAnalysis/stats/process_data.py
+++ CalciumAnalysis/stats/process_data.py
@@ -19,18 +19,15 @@ from sklearn.preprocessing import StandardScaler
 
 
 class ProcessData(object):
-
     def __init__(self, data):
         assert isinstance(data, CalciumData)
-        self.tracedata = data.tracedata
-        self.time = data.time
-        self.cells = data.cells
-        self.licktime = data.eventdata.loc[
-            data.eventdata['Lick'] == 1, 'time']
+        self.signals = data.tracedata.signals
+        self.time = data.tracedata.time
+        self.cells = data.tracedata.cells
         self.get_antibouts()
         self.session = data.session
-        self.trial_times = data.trial_times
-
+        self.trial_times = data.eventdata.trial_times
+        self.timestamps = data.eventdata.timestamps
         self.get_antibouts()
         self.sponts = self.get_sponts()
 
@@ -53,20 +50,20 @@ class ProcessData(object):
 
     def get_antibouts(self) -> pd.DataFrame | pd.Series:
         antibouts = pd.DataFrame()
-        for interv in funcs.interval(self.licktime, gap=10, outer=True):
-            df = self.tracedata.loc[
-                (self.tracedata['time'] > (interv[0])) &
-                (self.tracedata['time'] < (interv[1]))]
+        for interv in funcs.interval(self.timestamps['Lick'], gap=10, outer=True):
+            df = self.signals.loc[
+                (self.time > (interv[0])) &
+                (self.time < (interv[1]))]
             antibouts = pd.concat([antibouts, df], axis=0)
             antibouts.sort_values(by='time')
         return antibouts
 
     def get_sponts(self) -> pd.DataFrame | pd.Series:
         sponts = pd.DataFrame()
-        for interv in funcs.interval(self.licktime, gap=30, outer=True):
-            df = self.tracedata.loc[
-                (self.tracedata['time'] > (interv[0])) &
-                (self.tracedata['time'] < (interv[1]))]
+        for interv in funcs.interval(self.timestamps['Lick'], gap=30, outer=True):
+            df = self.signals[
+                (self.time > (interv[0])) &
+                (self.time < (interv[1]))]
             sponts = pd.concat([sponts, df], axis=0)
             sponts.sort_values(by='time')
         return sponts
@@ -78,7 +75,6 @@ class ProcessData(object):
                              pd.DataFrame,
                              pd.DataFrame]:
 
-        firstcell = self.tracedata.columns[1]
         stats_df = pd.DataFrame(columns=[
             'File', 'Cell', 'Stimulus',
             'Trial', 'Baseline (mean)',
@@ -93,107 +89,84 @@ class ProcessData(object):
         raw_df = pd.DataFrame(columns=self.cells)
         count = 0
         stats_list = []
-        for cell in self.tracedata.columns[1:]:
+        for cell in self.signals.columns:
             count += 1
             cell_list = []
-
             for stim, times in self.trial_times.items():
                 ntrials = len(times)
-
                 trial_list = []
                 for iteration, trial in enumerate(times):
-
                     # Index to analysis window
                     data_ind = np.where(
                         (self.time > trial) & (self.time < trial + 5))[0]
-
                     # Index to baseline window (current: 4s pre stim)
                     bltime_ind = np.where(
                         (self.time > trial - 4) & (self.time < trial))[0]
                     bltime = self.time[bltime_ind]
                     bltime = [min(bltime), max(bltime)]
-
                     # Baseline statistics
-                    bl_signal = np.array(self.tracedata.iloc[bltime_ind, count])
+                    bl_signal = np.array(self.signals[bltime_ind, count])
                     mean_bl = np.mean(bl_signal)
                     stdev_bl = np.std(bl_signal)
                     c_interv = mean_bl + stdev_bl * 2.58
-
                     # Get calcium trace & time
-                    signal = np.array(self.tracedata.iloc[data_ind, count])
-
+                    signal = np.array(self.signals[data_ind, count])
                     # Get peak signal & time
                     peak_signal = max(signal)
-                    peak_ts = self.tracedata.loc[self.tracedata[cell]
+                    peak_ts = self.signals[self.signals[cell]
                                                  == peak_signal, 'time'].iloc[0]
-
                     if peak_ts <= trial + 2.4:
                         peak_ts = max(bltime) + 2.4
                         shift = 'yes'
                     else:
                         shift = 'no'
-
                     # Get window 1s centered at peak
                     peak_window_ind = funcs.get_peak_window(self.time, peak_ts)
                     time_lower = self.time[peak_window_ind[0]]
                     time_upper = self.time[peak_window_ind[1]]
-                    response_window = np.array(self.tracedata.loc[
+                    response_window = np.array(self.signals[
                                                peak_window_ind[0]:peak_window_ind[1], cell])
-
                     window_ts = [time_lower, time_upper]
                     mean_mag = np.mean(response_window)
                     dff = ((mean_mag - mean_bl) / mean_bl)
-
                     # if DF/F is significant, output concat new dataframe
                     # with raw values from this trial
                     if dff >= c_interv:
-
                         d = [stim, 'Trial {}'.format(iteration + 1)]
                         d.extend(
-                            ['-' for _ in range(len(self.tracedata.columns[2:]))])
-
+                            ['-' for _ in range(len(self.signals[1:]))])
                         track = len(raw_df)
-
                         sig = 'Significant'
                         raw_df.loc[track] = d
-
-                        baseline_df = (self.tracedata[self.tracedata['time'].between(
+                        baseline_df = (self.signals[self.time.between(
                             min(bltime), time_upper)])
                         raw_df = pd.concat([raw_df, baseline_df])
-
                     else:
                         sig = 'ns'
-
                     # Check if peak_signal is a duplicate
                     for _ in signal:
                         funcs.dup_check(signal, peak_signal)
-
                     # Pack stats into list
                     iteration_list = [self.session, cell, stim,
                                       f'Trial {iteration + 1}',
                                       mean_bl, stdev_bl, window_ts,
                                       bltime, shift, dff, sig]
                     trial_list.append(iteration_list)
-
-                if cell == firstcell:
+                if cell == self.signals[0]:
                     summary_list = [
                         self.session,
                         stim,
-                        ntrials
-                    ]
+                        ntrials]
                     track = len(summary)
                     summary.loc[track] = summary_list
-
                 cell_list.append(trial_list)
             stats_list.append(cell_list)
         flaten_stats = funcs.flatten(funcs.flatten(stats_list))
-
         # Make dataframe from flattened list of stats
         for ind, d in enumerate(flaten_stats):
             track = len(stats_df)
             stats_df.loc[track] = d
         logging.info('Stats successfully completed.')
-
         if output:
             print("Outputting data to Excel...")
             with pd.ExcelWriter(str(savedir)
@@ -207,7 +180,5 @@ class ProcessData(object):
                 #     writer, sheet_name='Lick cells', index=False)
                 stats_df.to_excel(
                     writer, sheet_name='Trial Stats', index=False)
-
-            print(' statistics successfully transfered to Excel!')
-
+            print(' statistics successfully transferred to Excel!')
         return stats_df, raw_df, summary
