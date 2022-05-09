58a8be90fb869cb01a5ae842ef3537869d2e8d0e
Flynn P O'Connell <oconnell@binghamton.edu>
2022-05-09 15:14:52 -0400

    add config import

--- CalciumAnalysis/__init__.py
+++ CalciumAnalysis/__init__.py
@@ -5,5 +5,13 @@
 
 CalciumAnalysis package for processing acquired calcium traces and corresponding events.
 """
+from pathlib import Path
+import yaml
 
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
+my_path = Path(__file__).parent.resolve()  # resolve to get rid of any symlinks
+config_path = my_path.parent / 'config.yaml'
+
+with config_path.open() as config_file:
+    config = yaml.safe_load(config_file)
