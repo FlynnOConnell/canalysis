a5a0561ea6836652aca2dc24ff012cb95bcde693
Flynn P O'Connell <oconnell@binghamton.edu>
2022-05-14 17:21:17 -0400

    rename

--- CalciumAnalysis/neuralnetwork/nn_utils/_properties.py
+++ CalciumAnalysis/neuralnetwork/nn_utils/_properties.py
@@ -1,41 +1,40 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-# _props.py
+# _properties.py
 
 Module(nn_utils): Boilerplate property getter/setters for SVM model.
     
 """
 from neuralnetwork.nn_utils._validate import _validate
 from neuralnetwork.nn_utils.datahandler import DataHandler
+from typing import Any
 
 
 class _props:
+    _evaldata: DataHandler
+    _cv: Any
+
     @property
     def evaldata(self):
         return self._evaldata
-    
+
     @evaldata.setter
     def evaldata(self, handlerargs):
-        try:
-            data, target, stage = handlerargs
-        except: 
-            raise AttributeError('Must pass an iterable with 3 values')
-        self._evaldata = DataHandler(data=data, target=target, stage=stage)
-        print(f'Eval data set: {handlerargs}')
-       
+        data, target, stage = handlerargs
+
     @property
     def cv(self):
         return self._cv
-    
+
     @cv.setter
     def cv(self, new_cv):
         self._cv = new_cv
-        
+
     @property
     def model(self):
         return self._model
-    
+
     @model.setter
     def model(self, new_model):
         if new_model:
