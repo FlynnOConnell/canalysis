164b58d900fafcafc823032bf1b0edcf2eee05ff
Flynn P O'Connell <oconnell@binghamton.edu>
2022-05-14 17:20:36 -0400

    remove split checker

--- CalciumAnalysis/neuralnetwork/nn_utils/_validate.py
+++ CalciumAnalysis/neuralnetwork/nn_utils/_validate.py
@@ -10,43 +10,29 @@ Module (nn_utils):
 
 import numpy as np
 
+
 class _validate:
-    def _is_split(self):
-        if 'X_train' not in self.trainset.keys():
-            raise AttributeError('No train/test splits have been provided')
-    
     @staticmethod
-    def _full_predict(pred,true):
+    def _full_predict(pred, true):
         assert (np.unique(pred)) == (np.unique(true))
-                    
+
     @staticmethod
     def _validate_shape(x, y):
         assert x.shape[0] == y.shape[0]
-    
+
     @staticmethod
     def _check_fitted(model):
         if not hasattr(model, "fit"):
             raise TypeError(f"{model} is not an estimator instance.")
-            
+
     @staticmethod
     def _num_classes(classes):
         if len(np.unique(classes)) == 2:
             print('Estimator class type: Multivariate')
             return 'binary'
-        elif len(np.unique(classes)) > 2: 
+        elif len(np.unique(classes)) > 2:
             print('Estimator class type: Multivariate')
             return "multivariate"
-        else: 
+        else:
             raise AttributeError('Number of classes do not match binary or multivariate')
 
-    def _check_classes(self, classes):
-        """Check number of classes match"""
-        assert self._num_classes(classes) == self.model_type
-        
-        
-                
-
-
-            
-        
-        
