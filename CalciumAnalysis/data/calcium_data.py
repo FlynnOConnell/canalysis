e68bd95209afb025feffdc8cc83ae9d0eca968a2
Flynn OConnell <oconnell@binghamton.edu>
2022-06-16 01:02:33 -0400

    clean imports, dict handling

--- CalciumAnalysis/data/calcium_data.py
+++ CalciumAnalysis/data/calcium_data.py
@@ -10,15 +10,16 @@ from __future__ import annotations
 import logging
 from dataclasses import dataclass, field
 from typing import ClassVar
-from misc import funcs
-from data.data_utils.file_handler import FileHandler
 
-from all_data import AllData
-from trace_data import TraceData
-from taste_data import TasteData
-from event_data import EventData
+from .data_utils.file_handler import FileHandler
+
+from .all_data import AllData
+from .trace_data import TraceData
+from .taste_data import TasteData
+from .event_data import EventData
 from graphs.graph_utils import Mixins
-from misc import excepts as e
+from utils import excepts as e
+from utils import funcs
 logger = logging.getLogger(__name__)
 
 
@@ -28,10 +29,11 @@ logger = logging.getLogger(__name__)
 class CalciumData(Mixins.CalPlots):
 
     filehandler: FileHandler
+    color_dict: color_dict
     tracedata: TraceData = field(init=False)
     eventdata: EventData = field(init=False)
     _tastedata: TasteData = field(init=False)
-    color_dict = FileHandler
+
     alldata: ClassVar[AllData] = AllData.Instance()
 
     def __post_init__(self):
@@ -70,15 +72,18 @@ class CalciumData(Mixins.CalPlots):
         return TraceData(self.filehandler)
 
     def _set_eventdata(self) -> EventData:
-        return EventData(self.filehandler)
+        return EventData(self.filehandler, self.color_dict)
 
     def _authenticate(self):
         if not isinstance(self.tracedata, TraceData):
             raise e.DataFrameError('Trace data must be a dataframe')
         if not isinstance(self.eventdata, EventData):
             raise e.DataFrameError('Event data must be a dataframe.')
-        if not any(x in self.tracedata.signals.columns for x in ['C0', 'C00']):
-            raise AttributeError("No cells found in DataFrame")
+        if not any(x in self.tracedata.signals.columns for x in ['C0', 'C00', 'C000',
+                                                                 'C0000']):
+            logging.debug(f'{self.tracedata.signals.head()}')
+            raise AttributeError(f"No cells found in DataFrame: "
+                                 f"{self.tracedata.signals.head()}")
         return None
 
     def _add_instance(self):
