051f0304bf6c590aeefc47273f36d9bd6051cb2b
Flynn OConnell <oconnell@binghamton.edu>
2022-08-19 10:27:56 -0400

    Update name

---
+++ CalciumAnalysis/analysis/analysis_utils/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# ./analysis/analysis_utils.__init__.py
+
+Subpackage for statistical helper functionis and libraries.
+"""
+name = "analysis_utils"
