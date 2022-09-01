1b3b0c49663e1e53fba5d08f8f42990888d773c6
Flynn P O'Connell <oconnell@binghamton.edu>
2022-08-31 22:15:39 -0400

    split ax/fig funcs

--- CalciumAnalysis/graphs/plot.py
+++ CalciumAnalysis/graphs/plot.py
@@ -16,10 +16,9 @@ import matplotlib.font_manager as fm
 import matplotlib.pyplot as plt
 import numpy as np
 
-from matplotlib import rcParams, figure
-import graphs.graph_utils.graph_funcs as gr_func
+from graph_utils import helpers, ax_helpers
+from graph_utils.cafigure import CalFigure
 import matplotlib
-from pathlib import Path
 
 matplotlib.use("TkAgg")
 logger = logging.getLogger(__name__)
@@ -29,22 +28,8 @@ logging.basicConfig(level=logging.INFO, format="%(name)s - %(message)s")
 _UNSPECIFIED = object()
 
 
-def update_rcparams():
-    # Function to set some easy params and avoid some annoying bugs
-    rcParams.update(
-        {
-            "font.weight": "bold",
-            "axes.labelweight": "bold",
-            "axes.facecolor": "w",
-            "axes.labelsize": 15,
-            "lines.linewidth": 1,
-            'animation.ffmpeg_path': r'/c/ffmpeg/bin/ffmpeg',
-            'scatter.edgecolors': None
-        })
-
-
 def get_figure(*args, ax=None, **kwargs):
-    update_rcparams()
+    helpers.update_rcparams()
     fig = plt.figure(FigureClass=CalFigure, *args, **kwargs)
     ax = ax if ax is not None else plt.gca()
     ax.set_facecolor = "w"
@@ -59,65 +44,26 @@ def get_axis_points(data):
         return [data.iloc[:, 0], data.iloc[:, 1]]
 
 
-def get_axis_labels(ax, data):
-    ax.set_xlabel(data.columns[0], weight="bold")
-    ax.set_ylabel(data.columns[1], weight="bold")
-    if data.shape[1] > 2:
-        ax.set_zlabel(data.columns[1], weight="bold")
-    return ax
-
-
-def get_legend(ax, color_dict, colors):
-    mydict = {k: v for k, v in color_dict.items() if color_dict[k] in np.unique(colors)}
-    proxy, label = gr_func.get_handles_from_dict(mydict, colors)
-    ax.legend(
-        handles=proxy,
-        labels=label,
-        prop={"size": 5},
-        bbox_to_anchor=(1.05, 1),
-        ncol=1,
-        numpoints=1,
-        facecolor=ax.get_facecolor(),
-        edgecolor='w')
-    return ax
-
-
-def pca_scatter(data, colors, color_dict=None, **kwargs):
+def pca_scatter(data, colors, color_dict=None, s: int = 10, **kwargs):
     proj = '3d' if data.shape[1] > 2 else 'rectilinear'
     ax = plt.axes(projection=proj)
-    ax = get_axis_labels(ax, data)
+    ax = ax_helpers.get_axis_labels(ax, data)
     ax.set_title(f"{'Principal Components'}", fontweight="bold")
     ax.scatter(
         *get_axis_points(data),
         c=colors,
+        s=s,
         marker='o',
         facecolor='w',
-        **kwargs, )
+        **kwargs,)
     if color_dict is not None:
-        ax = get_legend(ax, color_dict, colors)
+        ax = ax_helpers.get_legend(ax, color_dict, colors, s)
     fig, ax = get_figure(ax=ax, figsize=(3, 3))
     plt.show()
     return fig, ax
 
 
-class CalFigure(matplotlib.figure.Figure):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # self.set_dpi(dpi)
-        self.tight_layout()
-        self.frameon = False
-
-    def save(self, title='', *args, **kwargs):
-        self.savefig(
-            f"{title}.png",
-            bbox_inches='tight',
-            facecolor=self.get_facecolor(),
-            edgecolor=None,
-            *args,
-            **kwargs)
-
-
-def skree(variance: Sized, title: str = "") -> plt.figure:
+def pca_skree(variance: Sized, title: str = "") -> plt.figure:
     """
     Line chart skree plot.
 
@@ -150,67 +96,6 @@ def skree(variance: Sized, title: str = "") -> plt.figure:
     leg.get_frame().set_alpha(1)
     plt.show()
     return fig
-
-
-# def scatter_animated(
-#         url: str = r"C:\Users\flynn\Desktop\figs\temp.mp4",
-#         size: int = 5,
-#         marker: str = "o",
-#         alpha: int = 1,
-# ) -> HTML:
-#     """
-#         Animated 3D Scatter plot.
-#         Plot gets saved to a temporary html file (location provided by save_dir).
-#         Args:
-#             url (str): Save location.
-#             size (int): Size of graph markers. Default = 5.
-#             marker (str): Shape of marker. Default is circle.
-#             alpha (int): Alpha of markers.
-#
-#         Returns:
-#             None
-#     """
-#     fig = plt.figure()
-#     ax = fig.gca(projection="3d")
-#
-#     numcols = '3d' if data.shape[1] > 2 else 'rectilinear'
-#     fig = plt.figure(dpi=dpi, frameon=True, facecolor='white', projection=numcols)
-#     ax = plt.axes(projection=numcols)
-#     ax = get_axis_labels(ax)
-#     ax.set_title("3D - PCA")
-#     ax.scatter(
-#         *get_axis_points(),
-#         c=colors,
-#         s=size, marker=marker, alpha=alpha)
-#
-#     proxy, label = gr_func.get_handles_from_dict(self.color_dict)
-#     ax.legend(
-#         handles=proxy,
-#         labels=label,
-#         loc="upper right",
-#         prop={"size": 6},
-#         bbox_to_anchor=(1, 1),
-#         ncol=2,
-#         numpoints=1,
-#     )
-#
-#     def init():
-#         ax.plot(*self.get_axis_points(), linewidth=0, antialiased=False)
-#         return (fig,)
-#
-#     def animate(i):
-#         ax.view_init(elev=30.0, azim=3.6 * i)
-#         return (fig,)
-#
-#     ani = animation.FuncAnimation(
-#         fig, animate, init_func=init, frames=400, interval=100, blit=True
-#     )
-#
-#     data = HTML(ani.to_html5_video())
-#     with open(f'{url}', "wb") as f:
-#         f.write(data.data.encode("UTF-8"))
-#     webbrowser.open(url, new=2)
-#     return data
 
 
 def confusion_matrix(
