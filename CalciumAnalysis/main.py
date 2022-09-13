aa41fdfc8962edfae479dc8868272bc23b805c05
Flynn P O'Connell <oconnell@binghamton.edu>
2022-09-12 23:02:16 -0400

    adapt heatmaps

--- CalciumAnalysis/main.py
+++ CalciumAnalysis/main.py
@@ -8,19 +8,20 @@ Module: Main code execution.
 """
 from __future__ import annotations
 
+import faulthandler
 import logging
+import os
 
-from analysis.analysis_utils import ca_pca
+import pandas as pd
+import yaml
+
+import utils.funcs
+from analysis.principal_components import get_pca
+from analysis.process_data import ProcessData
 from calcium_data import CalciumData
 from data_utils.file_handler import FileHandler
-from containers.taste_data import TasteData
-from analysis.process_data import ProcessData
-import pandas as pd
-import faulthandler
-import utils.funcs
-from utils.wrappers import log_time
 from graphs.plot import pca_scatter
-import inspect
+from utils.wrappers import log_time
 
 faulthandler.enable()
 
@@ -28,21 +29,26 @@ logging.basicConfig(level=logging.INFO, format="%(message)s")
 logger = logging.getLogger(__name__)
 save_dir = r'C:\Users\flynn\Desktop\figs'
 # save_dir = "C:/Users/dilorenzo/Desktop/CalciumPlots/"
-# %% Some examples on usage of different modules.
 
-color_dict = {
-    "Peanut": "darkorange",
-    "Chocolate": "saddlebrown",
-    "NaCl": "green",
-    "Quinine": "red",
-    "Acid": "yellow",
-    "Sucrose": "purple",
-    "Approach": "slategray",
-    "Eating": "blue",
-    "Grooming": "cyan",
-    "Entry": "lime",
-    "Doing Nothing": "k"
-}
+__location__ = os.path.realpath(
+        os.path.join(
+                os.getcwd(),
+                os.path.dirname(__file__)))
+
+
+def get_params():
+    with open(os.path.join(__location__, 'params.yaml'),
+              'rb') as f:
+        parameters = yaml.safe_load(f.read())
+    return Params(parameters)
+
+
+class Params:
+    """@DynamicAttrs"""
+
+    def __init__(self, parameter_dict):
+        for key in parameter_dict:
+            setattr(self, key, parameter_dict[key])
 
 
 def reorder(tracedata):
@@ -52,9 +58,15 @@ def reorder(tracedata):
     tracedata.reorder(dflist)
 
 
+def pca(df, col):
+    mypca = get_pca(df3, numcomp=3).pca_df
+    scatter = pca_scatter(mypca, color3, color_dict=params.Colors, edgecolors=None, s=20)
+    return None
+
+
 @utils.wrappers.log_time
 def initialize_data(_filehandler: FileHandler, adjust: int = None):
-    return CalciumData(_filehandler, color_dict, adjust=adjust)
+    return CalciumData(_filehandler, color_dict=params.Colors, adjust=adjust)
 
 
 def statistics(_data, _dir) -> pd.DataFrame | None:
@@ -71,27 +83,41 @@ def heatmap_loops(_data, anal, cols):
 
 
 if __name__ == "__main__":
-    _animal = "PGT13"
-    _date = "052622"
-    _dir = r"C:\Users\flynn\repos\CalciumAnalysis\datasets"
+    params = get_params()
 
     filehandler = FileHandler(
-            _animal, _date, _dir, tracename="traces3", eatingname="Scored2"
+            params.Session['animal'],
+            params.Session['date'],
+            params.Directory['data'],
+            params.Filenames['traces'],
+            params.Filenames['events'],
+            params.Filenames['gpio'],
+            params.Filenames['eating']
     )
     data = initialize_data(filehandler, adjust=34)
 
     df, color = data.tastedata.get_signals_from_events(
-            ['Peanut', 'NaCl', 'Chocolate', 'Sucrose', 'Citric', 'Quinine']
-    )
+            ['Peanut', 'NaCl', 'Chocolate', 'Sucrose', 'Acid', 'Quinine'])
 
     df2, color2 = data.eatingdata.get_signals_from_events(
-            ['Grooming', 'Eating', 'Approach', 'Entry', 'Doing Nothing']
-    )
+            ['Grooming', 'Eating', 'Approach', 'Entry', 'Doing Nothing'])
 
-    x, xcolors = data.combine(['Eating', 'Approach', 'Entry', 'Doing Nothing'],
-                              ['Peanut', 'NaCl', 'Chocolate', 'Sucrose', 'Citric', 'Quinine'])
-    mypca = ca_pca.get_pca(x).pca_df
-    scatter = pca_scatter(mypca, xcolors, color_dict=color_dict, edgecolors=None, s=20)
-    import matplotlib.pyplot as plt
+    df3, color3 = data.combine(['Eating', 'Approach'],
+                               ['Peanut', 'NaCl', 'Chocolate', 'Sucrose', 'Acid', 'Quinine'])
 
-    x = inspect.signature(plt.figure)
+    hm = data.eatingdata.eating_heatmap(save_dir='', show=True)
+    for h in hm:
+        h = h
+
+
+
+
+
+
+
+
+
+
+
+
+# 'Eating', 'Approach', 'Entry', 'Doing Nothing',
