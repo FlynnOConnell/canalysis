0b3827faa26832279fdcb42d830152e308b50a07
Flynn P O'Connell <oconnell@binghamton.edu>
2022-09-12 23:00:28 -0400

    rm redundency

--- CalciumAnalysis/graphs/__init__.py
+++ CalciumAnalysis/graphs/__init__.py
@@ -1,19 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 19 20:39:48 2022
 
-@author: flynnoconnell
-"""
-from __future__ import annotations
-import logging
-import faulthandler
 
-from graph_utils import helpers, ax_helpers, cafigure
 
-faulthandler.enable()
-logging.basicConfig(level=logging.INFO, format="%(message)s")
-logger = logging.getLogger(__name__)
-
-__all__ = [helpers, ax_helpers, cafigure]
-print("Importing", __name__)
