9a960501092e6638349ee4208700b4062af7fe77
Flynn OConnell <flynnoconnell@gmail.com>
2025-05-15 20:22:35 -0400

    pub figs

---
+++ canalysis/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+A package for analysis, statistics and visualization of Calcium Imaging data,
+specifically from including from Inscopix.
+
+"""
+from __future__ import annotations
+
+import os
+from inspect import getsourcefile
+
+import yaml
+from canalysis import helpers
+from canalysis.data.containers import CalciumData
+from canalysis.data.data_utils.file_handler import FileHandler
+
+__location__ = os.path.abspath(getsourcefile(lambda: 0))
+ROOT_DIR = os.path.dirname(__location__)
+
+_hard_dependencies = ["numpy", "pandas"]  # let users know if theyre missing any vital deps
+_missing_dependencies = []
+
+for _dependency in _hard_dependencies:
+    try:
+        __import__(_dependency)
+    except ImportError as _e:
+        _missing_dependencies.append(f"{_dependency}: {_e}")
+
+if _missing_dependencies:
+    raise ImportError("Unable to import required dependencies:\n" + "\n".join(_missing_dependencies))
+del _hard_dependencies, _dependency, _missing_dependencies
+
+
+class Params:
+    """@DynamicAttrs"""
+
+    def __init__(self, parameter_dict):
+        for key in parameter_dict:
+            setattr(self, key, parameter_dict[key])
+
+
+def get_parameters():
+    with open(os.path.join(ROOT_DIR, "params.yaml"), "rb") as f:
+        parameters = yaml.safe_load(f.read())
+    return Params(parameters)
+
+
+def get_data():
+    params = get_parameters()
+    filehandler = FileHandler(
+        params.Session["animal"],
+        params.Session["date"],
+        params.Directory["data"],
+        params.Filenames["traces"],
+        params.Filenames["events"],
+        params.Filenames["gpio"],
+        params.Filenames["eating"],
+    )
+    return CalciumData(
+        filehandler,
+        doeating=params.Filenames["doeating"],
+        doevents=params.Filenames["doevents"],
+        color_dict=params.Colors,
+        adjust=params.Filenames["adjust"],
+    )
+
+
+__doc__ = """
+canalysis - A library for processing, manipulating, combining and visualizing Calcium 
+Imaging datasets
+=====================================================================
+
+**canalysis** is a Python package that helps to process multivariate datasets. Calcium 
+Imaging data can take many forms, generally 
+you have a .csv/excel file with traces (values of how bright group of pixels is on a 
+screen at any time), events (when a stimulus is presented
+relative to the trace times). This library provides a backbone to combine and correlate 
+these types in various ways. 
+
+Main Features
+-------------
+Here are just a few of the things that canalysis does well:
+
+  - Easy directory management to keep data clean and in the same place.
+  - Process data for NaN's, outliers, and other issues.
+  - Combine multiple datasets into one class for easy manipulations. 
+  - Visualize data in various ways, including heatmaps, traces, and events. 
+
+Calcium data can be very specific to the type of experiment being performed. This 
+library can be easily modified to fit the type of data 
+you need to process. 
+
+"""
+
+# Path: CalciumAnalysis/CalciumAnalysis/data/containers/calcium_data.py
+__all__ = [
+    "CalciumData",
+    "FileHandler",
+    "get_data",
+    "Params",
+    "graphs",
+    "heatmaps",
+]
---
+++ pub/.ipynb_checkpoints/v1-checkpoint.py
@@ -0,0 +1,29 @@
+import canalysis as ca
+from copy import deepcopy
+import suite2p
+import mbo_utilities as mbo
+import lbm_suite2p_python as lsp
+import fastplotlib as fpl
+from pathlib import Path
+import numpy as np
+import tifffile
+
+#%%
+
+fpath = Path().home().joinpath("diloren", "PGT13", "052622", "suite2p", "plane0")
+ops = np.load(fpath.joinpath("ops.npy"), allow_pickle=True).item()
+iscell = np.load(fpath.joinpath("iscell.npy"), allow_pickle=True)[:, 0].astype("bool")
+spks = np.load(fpath.joinpath("spks.npy"), allow_pickle=True)
+
+savepath = fpath.parent.parent.joinpath("./outputs")
+savepath.mkdir(exist_ok=True)
+
+#%%
+fraw, fneu, spks = lsp.load_traces(ops)
+
+data = ca.get_data()
+
+data.plot_stim(savepath=savepath.joinpath("stim.png"))
+data.plot_session(savepath=savepath.joinpath("session.png"))
+data.plot_cells(savepath=savepath.joinpath("cells.png"))
+data.plot_zoom(savepath=savepath.joinpath("zoom.png"))
